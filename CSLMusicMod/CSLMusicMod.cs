141c0159961254246cc55688ba311e8e772fe795
Ruman Gerst <ruman.gerst@uni-jena.de>
2017-05-27 14:43:40 +0200

    * Bugfix: Null reference exception

--- CSLMusicMod/CSLMusicMod.cs
+++ CSLMusicMod/CSLMusicMod.cs
@@ -15,7 +15,7 @@ namespace CSLMusicMod
     {
         public static System.Random RANDOM = new System.Random();
 
-        public const String VersionName = "Rewrite 1.1.6.0";
+        public const String VersionName = "Rewrite 1.1.6.1";
 
         private SettingsUI m_SettingsUI;
 
